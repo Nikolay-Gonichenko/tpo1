6c54bbb563c99691bfe677744f50a297b71ca1cc
Gonichenko Nikolay <nick-kg@yandex.ru>
2023-03-05 10:20:51 +0300

    init commit

---
+++ src/main/java/ru/itmo/tpo1/Tpo1Application.java
@@ -0,0 +1,13 @@
+package ru.itmo.tpo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itmo/tpo1/Tpo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.tpo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
